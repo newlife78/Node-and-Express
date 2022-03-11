112d4bb7cdae8c68177fd7abe13912bb35874469
newlife78 <newlife.2021@sapo.pt>
2022-03-11 12:23:28 +0000

    create get and post requests_2022-03-11

--- myApp.js
+++ myApp.js
@@ -1,19 +1,48 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+require("dotenv").config();
 
-let absolutePathHtml = __dirname + "/views/index.html";
-let absolutePathCss = __dirname + "/public";
-
-app.use("/public", express.static(absolutePathCss));
-
-// app.get("/", (req, res) => {
-//   res.send("Hello Express");
-// });
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(absolutePathHtml);
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-console.log("Hello Node and Express!");
+app.get("/json", (req, res) => {
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json",
+  });
+});
+
+//Chain middleware to create a time server
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+
+//GET route parameters input from the client using an echo server
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
+});
+
+//GET query parameter input from the client
+app.get("/name", (req, res) => {
+  res.json({ name: req.query.first + " " + req.query.last });
+});
+
+//Get Data from POST Requests
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/name", (req, res) => {
+  res.json({ name: req.body.first + " " + req.body.last });
+});
 
 module.exports = app;
